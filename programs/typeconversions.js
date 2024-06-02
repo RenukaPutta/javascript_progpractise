d8edf8edae100dec40e14fd71aa0a7b9db0ed814
Renuka Putta <131524991+RenukaPutta@users.noreply.github.com>
2024-06-02 13:43:05 +0000

    conversions of datatypes

---
+++ programs/typeconversions.js
@@ -0,0 +1,26 @@
+let s=10
+console.log(typeof(s))
+let m=Boolean(s)
+console.log(typeof(m))
+console.log(m)
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let someni=33
+let k=String(someni)
+console.log(typeof(k))
+console.log(k)
+
+
+//operationsssssssssssssssssssssssss
+let v=3
+let negavl= -v
+console.log(negavl)
+console.log(typeof(negavl))
+
+
+
+
