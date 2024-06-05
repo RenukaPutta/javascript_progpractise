b3f3506b992517294af106a2b228e08cd2ca5a1e
Renuka Putta <131524991+RenukaPutta@users.noreply.github.com>
2024-06-05 12:58:42 +0000

    arraysss

---
+++ programs/arrays.js
@@ -0,0 +1,25 @@
+const arr=[1,4,6,8]
+const s1=["rit","hit","kit"]
+console.log(arr)
+console.log(arr.length)
+
+const s2=new Array(12,3,44,66)
+console.log(s2)
+
+
+arr.push(10)
+//arr.pop()
+console.log(arr)
+
+
+arr.shift(2)
+console.log(arr)
+arr.unshift(6)
+console.log(arr)
+
+
+
+arr.slice(1,3)
+console.log(arr)
+arr.splice(1,3)
+console.log(arr)
