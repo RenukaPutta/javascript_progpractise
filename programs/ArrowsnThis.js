17ecf7fc62406eab53e8fcbc6c64acedb9a8d910
Renuka Putta <131524991+RenukaPutta@users.noreply.github.com>
2024-06-13 17:02:20 +0000

    commit arrowsthis

--- programs/ArrowsnThis.js
+++ programs/ArrowsnThis.js
@@ -6,3 +6,37 @@ Greetings:function(){
 }
 
 }
+//user.Greetings()
+
+user.name='Mim'
+
+user.Greetings()
+
+console.log(this)
+
+
+// function chai(){
+//     let username="satya"
+//     console.log(`${this.username},hi`)
+// }
+// chai()
+
+//arrow function
+let chai = () =>{
+    let username="satya"
+    console.log(`${this.username},hi`)
+}
+chai()
+
+//if we use {} we should use return keyword or else if we use () we dont need any return keyword
+
+
+let m =(num1,num2) => {
+    return num1+num2
+}
+
+console.log(m(1111,1000));
+
+//---------------------------
+let n=(k1,k2) =>(k1+k2)
+console.log(n(17,31));
